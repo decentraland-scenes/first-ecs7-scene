b10968ab31d011bdd736e21335b4ababd916987e
Leandro Mendoza <leandro@decentraland.org>
2022-06-28 11:45:31 -0300

    initial commit for adaption layer

--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,14 @@
-// import './@decentraland/CommunicationsController'
-// import './@decentraland/EnvironmentAPI'
-// import './@decentraland/EthereumController'
-// import './@decentraland/SocialController'
-// import './@decentraland/ParcelIdentity'
-// import './@decentraland/web3-provider'
-// import './@decentraland/Identity'
-// import './@decentraland/SignedFetch'
-// import './@decentraland/Players'
-// import './@decentraland/RestrictedActions'
-// import './@decentraland/PortableExperiences'
-import './cube-scene'
+import { getPatchedDecentralandInterface } from './DecentralandInterface'
+import { getGameData } from './getEcs6GameData'
+import { customEval } from './sandbox'
+
+async function start() {
+  const gamejs = await getGameData()
+  if (!gamejs) throw new Error(`Unable to fetch the ecs6 main script file.`)
+
+  const newDcl = await getPatchedDecentralandInterface()
+
+  await customEval(gamejs, { dcl: newDcl })
+}
+
+start().catch(dcl.error)
--- src/helper/systems.ts
+++
@@ -1,4 +0,0 @@
-export function addSystem<T>(fn: (dt: number, state: T) => void, initialState: T) {
-  const state = { ...initialState }
-  engine.addSystem((dt: number) => fn(dt, state))
-}
